59ecd8d8f9307d2ceab0580f96783919905c2b07
nachoanton16 <nacho1a3@gmail.com>
2018-05-08 18:11:25 +0200

    interfaz carga

--- interfaces/SplashScreen/src/pacman_carga/FSplashScreen.java
+++ interfaces/SplashScreen/src/pacman_carga/FSplashScreen.java
@@ -5,17 +5,24 @@
  */
 package pacman_carga;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ocalderon
  */
-public class FSplashScreen extends javax.swing.JFrame {
+public class FSplashScreen extends javax.swing.JFrame implements Runnable{
+    FSplashScreen fss = new FSplashScreen();
+    Thread hilo;
+    private Object pbSlashsScreen;
 
     /**
      * Creates new form FSplashScreen
      */
     public FSplashScreen() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -31,21 +38,24 @@ public class FSplashScreen extends javax.swing.JFrame {
         pbSplashScreen = new javax.swing.JProgressBar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/cargapacman.jpg"))); // NOI18N
         jLabel1.setText("SplashScreen");
+        jLabel1.setMaximumSize(new java.awt.Dimension(1280, 720));
+        jLabel1.setMinimumSize(new java.awt.Dimension(1280, 720));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1292, Short.MAX_VALUE)
             .addComponent(pbSplashScreen, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(pbSplashScreen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -56,40 +66,29 @@ public class FSplashScreen extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FSplashScreen().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JProgressBar pbSplashScreen;
     // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void run() {
+        this.setVisible(true);
+        WindowsUtil.makeWindowsOpacity(this, 0.6f);
+        
+        try {
+            for(int i=0; i<100; i++){
+                pbSlashsScreen.setValue(i);
+                hilo.sleep(50);
+            }
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(FSplashScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        this.dispose();
+        fss.setVisible(true);
+    }
 }
