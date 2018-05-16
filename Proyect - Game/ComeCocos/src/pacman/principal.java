41b4dd38c4665391cabf792fac236ec5a1505495
oscar calderon <gorri0302@gmail.com>
2018-05-16 12:20:54 +0200

    Interfaces

---
+++ Proyect - Game/ComeCocos/src/pacman/principal.java
@@ -0,0 +1,14 @@
+package pacman;
+
+import javax.swing.*;
+
+public class principal {
+    private JLabel principal;
+    private JButton elegirModoJuegoButton;
+    private JButton verPuntuacionesButton;
+    private JButton salirButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
