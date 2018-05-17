19aeb574de5dc1f137c768aabf687bc8740100cb
oscar calderon <gorri0302@gmail.com>
2018-05-17 08:57:56 +0200

    Añadidos botones

--- Proyect - Game/ComeCocos/src/pacman/elegirjuego.java
+++ Proyect - Game/ComeCocos/src/pacman/elegirjuego.java
@@ -1,4 +1,11 @@
 package pacman;
 
+import javax.swing.*;
+
 public class elegirjuego {
+    private JPanel panel1;
+    private JButton contrarrelojButton;
+    private JButton volverButton;
+    private JButton flappyBirdButton;
+    private JButton pacmanButton;
 }
--- Proyect - Game/ComeCocos/src/pacman/finaljuego.java
+++ Proyect - Game/ComeCocos/src/pacman/finaljuego.java
@@ -1,4 +1,9 @@
 package pacman;
 
+import javax.swing.*;
+
 public class finaljuego {
+    private JPanel panel1;
+    private JButton menuPrincipalButton;
+    private JButton salirButton;
 }
