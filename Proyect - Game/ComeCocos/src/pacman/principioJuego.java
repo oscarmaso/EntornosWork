16ca5899984f15b2fe477fd5c96c4ab40129c212
oscarcm99 <gorri0302@gmail.com>
2018-05-17 19:43:51 +0200

    clase botones interaccion

--- Proyect - Game/ComeCocos/src/pacman/principioJuego.java
+++ Proyect - Game/ComeCocos/src/pacman/principioJuego.java
@@ -1,10 +1,48 @@
 package pacman;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import java.awt.EventQueue;
 
 public class principioJuego {
     private JPanel panel1;
     private JButton elegirModoJuegoButton;
     private JButton verPuntuacionesButton;
     private JButton salirButton;
+
+    public principioJuego() {
+        elegirModoJuegoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        verPuntuacionesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+    /*public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                principioJuego principio = new principioJuego();
+                principio.setVisible(true);
+            }
+        });
+    }*/
 }
