6a9d61ac9198adf2b32f0b43a57e896ce581d34b
oscarcm99 <gorri0302@gmail.com>
2018-05-17 17:11:40 +0200

    interfaz puntuaciones

---
+++ Proyect - Game/ComeCocos/src/pacman/puntuaciones.java
@@ -0,0 +1,11 @@
+package pacman;
+
+import javax.swing.*;
+
+public class puntuaciones {
+    private JPanel panel1;
+    private JTextArea puntuacionesTextArea;
+    private JButton volverButton;
+    private JTextArea recordTextArea;
+    private JButton salirButton;
+}
